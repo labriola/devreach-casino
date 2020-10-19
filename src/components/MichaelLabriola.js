64ab419eec9007e93f9a001c93442c27260c3cb5
Michael Labriola <labriola@digitalprimates.net>
2020-10-19 18:48:27 -0500

    Working demo

--- src/components/MichaelLabriola.js
+++ src/components/MichaelLabriola.js
@@ -1,8 +1,77 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import {
+  gameReducer,
+  dealReducerFn,
+  hitReducerFn,
+  standReducerFn,
+  PLAYING
+} from './labriola';
 
-function MichaelLabriola() {
+const Controls = props => {
+  const {
+    dealOnClick,
+    hitOnClick,
+    standOnClick,
+    status
+  } = props;
+
   return (
-    <h2>Michael Labriola</h2>
+    <div className="blackjack-controls">
+      <button
+        className="blackjack-button"
+        onClick={dealOnClick}
+        disabled={status === PLAYING}>Deal</button>
+      <button
+        className="blackjack-button"
+        onClick={hitOnClick}
+        disabled={status !== PLAYING}>Hit</button>
+      <button
+        className="blackjack-button"
+        onClick={standOnClick}
+        disabled={status !== PLAYING}>Stand</button>
+    </div>
+  )
+};
+
+const Hand = ({ status, hand=[], isDealer = false }) => {
+  return hand.map(({ path }, index) => {
+    if (!status && isDealer && index === 0) {
+      return <img src={'/cards/back.png'} alt={path} key={path} />;
+    }
+    return <img src={`/cards/${path}`} alt={path} key={path} />;
+  });
+};
+
+const MichaelLabriola = () => {
+
+  const [state, dispatch] = useReducer(gameReducer, undefined, gameReducer);
+  const {
+    playerHand = [],
+    dealerHand = [],
+    status
+  } = state;
+
+  return (
+    <>
+      <h2>Michael Labriola</h2>
+      <section className="blackjack">
+        <div className="cards">
+          <Hand hand={dealerHand} status={status} isDealer={true} />
+        </div>
+        <div className="blackjack-status">
+          {status}
+        </div>
+        <Controls
+          dealOnClick={() => dispatch(dealReducerFn)}
+          hitOnClick={() => dispatch(hitReducerFn)}
+          standOnClick={() => dispatch(standReducerFn)}
+          status={status}
+        />
+        <div className="cards">
+          <Hand hand={playerHand} />
+        </div>
+      </section>
+    </>
   )
 }
 
---
+++ src/components/labriola.js
@@ -0,0 +1,124 @@
+const shuffle = require('shuffle-array');
+
+export const PLAYING = 'Playing';
+export const RESULT_PUSH = 'Push';
+export const RESULT_PLAYER_WIN = 'Player Win';
+export const RESULT_DEALER_WIN = 'Dealer Win';
+export const RESULT_PLAYER_BUST = 'Player Bust';
+export const RESULT_DEALER_BUST = 'Dealer Bust';
+
+const toPossibleScores = (...values) => {
+  return existingScores => existingScores.reduce((acc, score)=>{
+    return [...acc, ...values.map(value=>score + value)];
+  }, []);
+};
+
+const toCard = (name, toValueFn) => suit => {
+  return {
+    path: `${name}${suit}.png`,
+    toValueFn
+  };
+};
+
+const toDeck = () => {
+  const pipCards = Array.from({length: 9}, (v, i)=>i+2);
+  const courtCards = ['J', 'Q', 'K'];
+  const suits = ['C', 'D', 'H', 'S'];
+
+  return [
+    ...pipCards.reduce((acc, value)=>[...acc, ...suits.map(toCard(value, toPossibleScores(value)))], []),
+    ...courtCards.reduce((acc, name)=>[...acc, ...suits.map(toCard(name, toPossibleScores(10)))], []),
+    ...suits.map(toCard('A', toPossibleScores(1, 11)))
+  ];
+};
+
+const toInitialState = (decks=1) => {
+  return {
+    deck : Array.from({length: decks}, ()=>shuffle(toDeck())).reduce((acc, deck)=>[...acc, ...deck], [])
+  };
+};
+
+export const totalOf = (hand) => {
+  const possibleValues = hand.reduce((totals, {toValueFn}) => toValueFn(totals), [0]);
+
+  //find best outcome
+  return possibleValues.reduce((best, value)=>{
+    return (best > 21 || (value > best && value <=21)) ? value : best;
+  });
+};
+
+const toFinalStatus = (dealerTotal=0, playerTotal=0) => {
+  if (dealerTotal > 21) return RESULT_DEALER_BUST;
+  if (playerTotal > 21) return RESULT_PLAYER_BUST;
+  if (dealerTotal === playerTotal) return RESULT_PUSH;
+
+  return playerTotal > dealerTotal
+            ? RESULT_PLAYER_WIN
+            : RESULT_DEALER_WIN;
+};
+
+export const dealReducerFn = (s0={}) => {
+  const {
+    deck=[]
+  } = s0;
+
+  const [card1, card2, card3, card4, ...remainder] = deck;
+  const playerHand = [card1, card3];
+  const dealerHand = [card2, card4];
+
+  return {
+    ...s0,
+    playerHand,
+    dealerHand,
+    deck: remainder,
+    status: PLAYING
+  };
+};
+
+export const hitReducerFn = (s0={}) => {
+  const {
+    deck=[],
+    playerHand=[],
+    status: existingStatus
+  } = s0;
+
+  const [card, ...remainder] = deck;
+  const newPlayerHand = [...playerHand, card];
+
+  return {
+    ...s0,
+    playerHand: newPlayerHand,
+    deck: remainder,
+    status : totalOf(newPlayerHand) > 21 ? RESULT_PLAYER_BUST : existingStatus
+  };
+};
+
+export const standReducerFn = (s0={}) => {
+  const {
+    deck=[],
+    dealerHand=[],
+    playerHand=[]
+  } = s0;
+
+  const dealerTotal = totalOf(dealerHand);
+  if (dealerTotal < 17) {
+    const [card, ...remainder] = deck;
+    const newDealerHand = [...dealerHand, card];
+
+    return standReducerFn({
+      ...s0,
+      dealerHand: newDealerHand,
+      deck: remainder
+    });
+  }
+
+  return {
+    ...s0,
+    status : toFinalStatus(dealerTotal, totalOf(playerHand))
+  };
+};
+
+const identity = x => x;
+export const gameReducer = (s0=toInitialState(4), reducerFn=identity)=>{
+  return reducerFn(s0);
+};
